f99933a6da553048a88387199d58df04668729fd
jimthanhnobi <thahiostream@gmail.com>
2025-04-12 20:01:57 +0700

    Arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const ArrowFunction = () => {
+    console.log("Chu Van Thanh")
+  }
+
+  ArrowFunction()
 
   return (
     <>
