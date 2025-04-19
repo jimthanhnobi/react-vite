1a2c2196519c63d18487e91094e1702537874224
jimthanhnobi <thahiostream@gmail.com>
2025-04-19 23:33:26 +0700

    Add structure project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductPage from './pages/product.jsx';
+import "./styles/global.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/users",
+    element: <UserPage />,
+  },
+  {
+    path: "/products",
+    element: <ProductPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <App />
-)
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
+);
